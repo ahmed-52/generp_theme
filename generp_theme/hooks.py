10449f06055306018fa534147d71ccb5a5a490bf
Ahmed Abdulla <aaa384@cornell.edu>
2025-08-12 20:25:15 +0300

    fixez

--- generp_theme/hooks.py
+++
@@ -1,9 +0,0 @@
-app_name = "generp_theme"
-app_title = "GenERP Theme"
-app_publisher = "Ahmed"
-app_description = "Custom Desk/Workspace styling for ERPNext"
-app_email = "ahmedalgaithie@gmail.com"
-app_license = "MIT"
-
-
-app_include_css = "/assets/generp_theme/css/workspace.css"
---
+++ generptheme/hooks.py
@@ -0,0 +1,9 @@
+app_name = "generptheme"
+app_title = "GenRP Theme"
+app_publisher = "Ahmed"
+app_description = "Custom Desk Workspace Styles for ERPNext"
+app_email = "you@example.com"
+app_license = "MIT"
+
+# Applies your CSS globally to the Desk (backend UI)
+app_include_css = "/assets/generptheme/css/workspace.css"
